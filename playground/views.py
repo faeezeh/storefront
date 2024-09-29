8a124ee2eae36b18a9c4a4e0006b9923021ac97d
faeezeh <sadeghi_faeze@yahoo.com>
2024-09-29 19:10:50 +0330

    logging

--- playground/views.py
+++ playground/views.py
@@ -7,14 +7,20 @@ from rest_framework.views import APIView
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 import requests
+import logging
 
-
+logger = logging.getLogger(__name__)
 
 class HelloView(APIView):
-    @method_decorator(cache_page(5 * 60))
     def get(self, request):
-        response = requests.get('https://httpbin.org/delay/2')
-        data = response.json()
+
+        try:
+            logger.info('Calling httpbin')
+            response = requests.get('https://httpbin.org/delay/2')
+            logger.info('Received the response')
+            data = response.json()
+        except requests.ConnectionError:
+            logger.critical('httpbin is offline')
         return render(request, 'hello.html', {'name': 'data'})
 
 @cache_page(5 * 60)
--- storefront/settings.py
+++ storefront/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -151,6 +152,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
@@ -205,6 +208,33 @@ CACHES = {
         "TIMEOUT": 10 * 60 ,
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+LOGGING = {
+    'version' : 1 ,
+    'disable_existing_loggers' : False , 
+    'handlers' : {
+        'console' : {
+            'class' : 'logging.StreamHandler'
+        },
+        'file':{
+            'class':'logging.FileHandler',
+            'filename': 'general.log',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers':{
+        '':{
+            'handlers': ['console','file'],
+            'level': os.environ.get('DJANGO_LOG_LEVEL','INFO')
+        }
+    },
+    'formatters': {
+        'verbose': {
+            'format':'{asctime} ({levelname})-{name}-{message}',
+            'style': '{'
         }
     }
 }
